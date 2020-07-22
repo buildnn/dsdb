13e0f0133eb1a50c5dd8ffcf468d69c63949aa59
ggbaro <giacomo.barone@buildnn.com>
2020-07-22 16:50:37 +0200

    added flake8 exception

--- dsdb/__init__.py
+++ dsdb/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2018-2020 Giacomo Barone, Buildnn Team. All Rights Reserved.
 # MIT License
 
-from ._utils_dsdb import DsDbConnect, DsDb
+from ._utils_dsdb import DsDbConnect, DsDb  # noqa: F401
